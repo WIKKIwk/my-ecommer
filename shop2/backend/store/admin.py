47fd321fa2d47693fd478eaef69fcfd9d05459a2
WIKKI <uzkingshark@gmail.com>
2026-01-11 16:51:32 +0500

    Add Product admin

--- shop2/backend/store/admin.py
+++ shop2/backend/store/admin.py
@@ -7,3 +7,10 @@ from .models import Category, Product, Cart, CartItem, Order, OrderItem
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['name', 'slug', 'created_at']
     prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'category', 'price', 'stock', 'available']
+    list_filter = ['category', 'available']
+    prepopulated_fields = {'slug': ('name',)}
