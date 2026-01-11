cfe9a2d3e210a2b42d272c32d9ccebccbb201441
WIKKI <uzkingshark@gmail.com>
2026-01-11 16:49:50 +0500

    Add CartItem model

--- shop2/backend/store/models.py
+++ shop2/backend/store/models.py
@@ -40,3 +40,15 @@ class Cart(models.Model):
         return f'Cart {self.id}'
 
 
+class CartItem(models.Model):
+    """Item in cart"""
+    cart = models.ForeignKey(Cart, related_name='items', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    added_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.product.name} x{self.quantity}'
+
+
+
