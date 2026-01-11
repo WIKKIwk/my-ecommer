fffb39e5dc5108c60ea0eb5c3b57499222a54746
WIKKI <uzkingshark@gmail.com>
2026-01-11 16:48:23 +0500

    Add static and media file settings

--- shop2/backend/config/settings.py
+++ shop2/backend/config/settings.py
@@ -60,3 +60,15 @@ DATABASES = {
         'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
+
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'UTC'
+USE_I18N = True
+USE_TZ = True
+
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
