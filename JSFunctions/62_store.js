d1565c495756532561b8d5b0b9764d96e2670c93
WIKKI <uzkingshark@gmail.com>
2026-01-28 14:08:14 +0500

    Add simple state store

---
+++ JSFunctions/62_store.js
@@ -0,0 +1,28 @@
+/**
+ * Simple State Store
+ */
+
+class Store {
+  constructor(initialState = {}) {
+    this.state = initialState;
+    this.listeners = [];
+  }
+
+  getState() {
+    return this.state;
+  }
+
+  setState(newState) {
+    this.state = { ...this.state, ...newState };
+    this.listeners.forEach(listener => listener(this.state));
+  }
+
+  subscribe(listener) {
+    this.listeners.push(listener);
+    return () => {
+      this.listeners = this.listeners.filter(l => l !== listener);
+    };
+  }
+}
+
+module.exports = Store;
