3fc852ac91fef79b6a73ee9a4159b437779c582c
WIKKI <uzkingshark@gmail.com>
2026-01-28 15:14:50 +0500

    Add simple deep equality utility

---
+++ JSFunctions/151_is_equal.js
@@ -0,0 +1,23 @@
+/**
+ * Simple Deep Equality
+ */
+
+/**
+ * Check if two values are deeply equal
+ * @param {*} a 
+ * @param {*} b 
+ * @returns {boolean}
+ */
+function isEqual(a, b) {
+  if (a === b) return true;
+  if (a instanceof Date && b instanceof Date) return a.getTime() === b.getTime();
+  if (!a || !b || (typeof a !== 'object' && typeof b !== 'object')) return a === b;
+  if (a.prototype !== b.prototype) return false;
+  
+  const keys = Object.keys(a);
+  if (keys.length !== Object.keys(b).length) return false;
+  
+  return keys.every(k => isEqual(a[k], b[k]));
+}
+
+module.exports = { isEqual };
