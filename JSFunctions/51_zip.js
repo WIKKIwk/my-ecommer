2477307f49dbc5f878c0bf1cb7c7fdf11e0f71f2
WIKKI <uzkingshark@gmail.com>
2026-01-28 13:39:54 +0500

    Add zip utility

---
+++ JSFunctions/51_zip.js
@@ -0,0 +1,21 @@
+/**
+ * Zip Utility
+ */
+
+/**
+ * Zip arrays together
+ * @param {...Array} arrays 
+ * @returns {Array} Array of tuples
+ */
+function zip(...arrays) {
+  const minLength = Math.min(...arrays.map(arr => arr.length));
+  const result = [];
+  
+  for (let i = 0; i < minLength; i++) {
+    result.push(arrays.map(arr => arr[i]));
+  }
+  
+  return result;
+}
+
+module.exports = { zip };
