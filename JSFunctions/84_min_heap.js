1f555dcfe6c5821c0e1b4418d8d9467d51afe4c4
WIKKI <uzkingshark@gmail.com>
2026-01-28 14:10:40 +0500

    Add Min Heap data structure

---
+++ JSFunctions/84_min_heap.js
@@ -0,0 +1,57 @@
+/**
+ * Min Heap
+ */
+
+class MinHeap {
+  constructor() {
+    this.heap = [];
+  }
+
+  getParentIndex(i) { return Math.floor((i - 1) / 2); }
+  getLeftChildIndex(i) { return 2 * i + 1; }
+  getRightChildIndex(i) { return 2 * i + 2; }
+
+  swap(i1, i2) {
+    [this.heap[i1], this.heap[i2]] = [this.heap[i2], this.heap[i1]];
+  }
+
+  insert(key) {
+    this.heap.push(key);
+    this.heapifyUp();
+  }
+
+  heapifyUp() {
+    let index = this.heap.length - 1;
+    while (this.getParentIndex(index) >= 0 && this.heap[this.getParentIndex(index)] > this.heap[index]) {
+      this.swap(this.getParentIndex(index), index);
+      index = this.getParentIndex(index);
+    }
+  }
+
+  extractMin() {
+    if (this.heap.length === 0) return null;
+    if (this.heap.length === 1) return this.heap.pop();
+    
+    const min = this.heap[0];
+    this.heap[0] = this.heap.pop();
+    this.heapifyDown();
+    return min;
+  }
+
+  heapifyDown() {
+    let index = 0;
+    while (this.getLeftChildIndex(index) < this.heap.length) {
+      let smallerChildIndex = this.getLeftChildIndex(index);
+      if (this.getRightChildIndex(index) < this.heap.length && this.heap[this.getRightChildIndex(index)] < this.heap[smallerChildIndex]) {
+        smallerChildIndex = this.getRightChildIndex(index);
+      }
+      
+      if (this.heap[index] < this.heap[smallerChildIndex]) break;
+      
+      this.swap(index, smallerChildIndex);
+      index = smallerChildIndex;
+    }
+  }
+}
+
+module.exports = MinHeap;
