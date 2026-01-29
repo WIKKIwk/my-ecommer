040af6d556e26c8a0e467386288a38c957ed7ed7
WIKKI <uzkingshark@gmail.com>
2026-01-29 11:01:43 +0500

    Add Vector2D

---
+++ JSFunctions/89_vector2d.js
@@ -0,0 +1,34 @@
+/**
+ * Vector 2D Math
+ */
+
+class Vector2D {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    add(v) {
+        return new Vector2D(this.x + v.x, this.y + v.y);
+    }
+
+    sub(v) {
+        return new Vector2D(this.x - v.x, this.y - v.y);
+    }
+
+    mult(n) {
+        return new Vector2D(this.x * n, this.y * n);
+    }
+
+    mag() {
+        return Math.sqrt(this.x * this.x + this.y * this.y);
+    }
+
+    normalize() {
+        const m = this.mag();
+        if (m === 0) return new Vector2D(0, 0);
+        return this.mult(1 / m);
+    }
+}
+
+module.exports = Vector2D;
