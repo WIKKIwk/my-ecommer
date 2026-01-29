199978d9ac76060b852912d2450dcc261c25e1e1
WIKKI <uzkingshark@gmail.com>
2026-01-29 11:01:19 +0500

    Add Trie

---
+++ JSFunctions/83_trie.js
@@ -0,0 +1,38 @@
+/**
+ * Trie (Prefix Tree)
+ */
+
+class TrieNode {
+    constructor() {
+        this.children = {};
+        this.isEndOfWord = false;
+    }
+}
+
+class Trie {
+    constructor() {
+        this.root = new TrieNode();
+    }
+
+    insert(word) {
+        let current = this.root;
+        for (const char of word) {
+            if (!current.children[char]) {
+                current.children[char] = new TrieNode();
+            }
+            current = current.children[char];
+        }
+        current.isEndOfWord = true;
+    }
+
+    search(word) {
+        let current = this.root;
+        for (const char of word) {
+            if (!current.children[char]) return false;
+            current = current.children[char];
+        }
+        return current.isEndOfWord;
+    }
+}
+
+module.exports = Trie;
