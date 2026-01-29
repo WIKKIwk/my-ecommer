db1a73c242c8d3f5e2454726de1f6f8916c3b43e
WIKKI <uzkingshark@gmail.com>
2026-01-29 10:10:45 +0500

    Add debounce and throttle utilities

---
+++ JSFunctions/07_debounce_throttle.js
@@ -0,0 +1,38 @@
+/**
+ * Debounce and Throttle utilities
+ */
+
+/**
+ * Debounce function
+ * @param {Function} func - Function to debounce
+ * @param {number} wait - Wait time in ms
+ * @returns {Function} Debounced function
+ */
+function debounce(func, wait) {
+    let timeout;
+    return function (...args) {
+        const context = this;
+        clearTimeout(timeout);
+        timeout = setTimeout(() => func.apply(context, args), wait);
+    };
+}
+
+/**
+ * Throttle function
+ * @param {Function} func - Function to throttle
+ * @param {number} limit - Limit in ms
+ * @returns {Function} Throttled function
+ */
+function throttle(func, limit) {
+    let inThrottle;
+    return function (...args) {
+        const context = this;
+        if (!inThrottle) {
+            func.apply(context, args);
+            inThrottle = true;
+            setTimeout(() => inThrottle = false, limit);
+        }
+    };
+}
+
+module.exports = { debounce, throttle };
